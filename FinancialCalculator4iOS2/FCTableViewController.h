a42fff93287cbe34285cfe83412bdfcd3af30c17
maizhibin <maizhibin79>
2014-04-16 07:51:22 +0800

    no commit

---
+++ FinancialCalculator4iOS2/FCTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FCTableViewController.h
+//  FinancialCalculator4iOS2
+//
+//  Created by 麦志斌 on 14-4-16.
+//  Copyright (c) 2014年 letuu.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCTableViewController : UITableViewController
+
+@end
--- FinancialCalculator4iOS2/FCViewController.m
+++ FinancialCalculator4iOS2/FCViewController.m
@@ -8,16 +8,20 @@
 
 #import "FCViewController.h"
 
-@interface FCViewController ()
+@interface FCViewController  ()
 
 @end
 
 @implementation FCViewController
 
+
+NSArray *recipes;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    recipes = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +29,25 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [recipes count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [recipes objectAtIndex:indexPath.row];
+    return cell;
+}
+
 
 @end
